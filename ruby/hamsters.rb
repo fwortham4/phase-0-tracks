05cd2507fdf4cc730371ddc358aa52e5810c3eca
fwortham4 <forrest.wortham@gmail.com>
2016-05-03 18:43:58 -0500

    Corrected the input types (integer, boolean, string)

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,11 +3,15 @@ hamster_age = ""
 puts "Hamster's Name?"
 hamster_name = gets.chomp
 puts "Hamster Volume? (1-10)"
-hamster_volume = gets.chomp
+hamster_volume = gets.chomp.to_i
 puts "Hamster's fur color?"
 hamster_fur = gets.chomp
 puts "Is this a good hamster for adoption? (Yes/No)"
-hamster_adoptable = gets.chomp
+hamster_adoptable = gets.chomp.downcase
+if hamster_adoptable == "yes"
+  hamster_adoptable = true
+else hamster_adoptable = false
+end
 puts "What is the hamster's (estimated) age?"
-hamster_age = gets.chomp
+hamster_age = gets.chomp.to_i
 
