1545c79816a7a790f8748e1b6007d006b391c7e0
fwortham4 <forrest.wortham@gmail.com>
2016-05-03 18:35:20 -0500

    Added hamster variables in Release1

---
+++ ruby/hamsters.rb
@@ -0,0 +1,13 @@
+hamster_age = ""
+
+puts "Hamster's Name?"
+hamster_name = gets.chomp
+puts "Hamster Volume? (1-10)"
+hamster_volume = gets.chomp
+puts "Hamster's fur color?"
+hamster_fur = gets.chomp
+puts "Is this a good hamster for adoption? (Yes/No)"
+hamster_adoptable = gets.chomp
+puts "What is the hamster's (estimated) age?"
+hamster_age = gets.chomp
+
