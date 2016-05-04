9841f62e5b2cacd704b14edae0e8a0362eb1745e
fwortham4 <forrest.wortham@gmail.com>
2016-05-03 19:15:21 -0500

    Added readability to hamster records

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,7 +1,7 @@
 hamster_age = ""
 
 puts "Hamster's Name?"
-hamster_name = gets.chomp
+hamster_name = gets.chomp.capitalize
 puts "Hamster Volume? (1-10)"
 hamster_volume = gets.chomp.to_i
 puts "Hamster's fur color?"
@@ -14,4 +14,12 @@ else hamster_adoptable = false
 end
 puts "What is the hamster's (estimated) age?"
 hamster_age = gets.chomp.to_i
+if hamster_age == 0
+  hamster_age = "Age Unknown"
+end
 
+puts "#{hamster_name}'s record"
+puts "Fur Color: #{hamster_fur}"
+puts "Approximate Age: #{hamster_age}"
+puts "Night Volume: #{hamster_volume}"
+puts "Suitable for Adoption: #{hamster_adoptable}"
