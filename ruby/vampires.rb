0c8e00e13321ab1ea527b2f54505cdbf835e5f83
fwortham4 <forrest.wortham@gmail.com>
2016-05-07 17:06:33 -0500

    Fixed vampire name condition.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,30 +9,37 @@ birth_location = gets.chomp.downcase
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
 garlic = gets.chomp.downcase
-if garlic == "yes"
-  garlic = true
-else garlic = false
+case garlic
+  when 'y','Y','yes'
+    garlic = true
+  when 'n', 'N','no'
+    garlic = false
 end
 
 puts "Would you like to enroll in the company’s health insurance? (Yes or No)"
 health_insurance = gets.chomp.downcase
-if health_insurance == "yes"
-  health_insurance = true
-else health_insurance = false
+case health_insurance
+  when 'y','Y','yes'
+    health_insurance = true
+  when 'n', 'N','no'
+    health_insurance = false
 end
+
+puts "Name: #{name}"
+puts "Age: #{age}"
+puts "Birth Location: #{birth_location}"
+puts "Garlic?: #{garlic}"
+puts "Health Insurance?: #{health_insurance}"
 
 if age<=100 && (garlic == true || health_insurance == true) == true
   puts "Probably NOT a vampire."
-end
-
-if age>100 && (garlic == true || health_insurance == false) == true
+elsif age>100 && (garlic == true || health_insurance == false) == true
   puts "Probably a vampire."
-end
-
-if age>100 && (garlic == false && health_insurance == false) == true
+elsif age>100 && (garlic == false && health_insurance == false) == true
   puts "Almost certainly a vampire."
-end
-
-if name = "drake cula" || "tu fang" == true
-  puts "Definitely a vampire."
+elsif case name
+  when 'drake cula','tu fang'
+    puts "Definitely a vampire!!!"
+    end
+else puts "Result are inconclusive."
 end
