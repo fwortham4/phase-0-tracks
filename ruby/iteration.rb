e16090575794982560c778859af6996d07d8cc27
fwortham4 <forrest.wortham@gmail.com>
2016-05-10 19:44:42 -0500

    Completed Release1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,3 +7,25 @@
 # block{|x| puts x}
 # block{|x| puts x+1}
 
+primary_colors = [
+  "red",
+  "blue",
+  "yellow"
+]
+dish = {
+  spaghetti: "italian",
+  burrito: "mexican",
+  eggroll: "chinese"
+}
+
+p primary_colors
+primary_colors.each {|x| puts "This primary color is #{x}"}
+p primary_colors
+
+p dish
+dish.each {|x, y| puts "#{x} is #{y} food."}
+p dish
+
+p primary_colors
+primary_colors.map! {|x| p x.upcase}
+p primary_colors
