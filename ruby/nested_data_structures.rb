e8fd61f750c253305bf14374e4d72bec74a2cc10
fwortham4 <forrest.wortham@gmail.com>
2016-05-12 18:13:01 -0500

    Corrected Nested Data structure: TN Sports

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,35 +2,37 @@
 #by Forrest Wortham
 #
 
-prof_sports = {
-football: {
-  teams: 'titans',
-  position: {
-    qb: 3,
-    rb: 5
-    ol: 9
-    wr: 11
-    }
-    }
+tn_sports = {
+  football:{
+      teams: 'titans',
+      position: [
+        qb: 3,
+        rb: 5,
+        ol: 9,
+        wr: 11
+      ]
+    },
 
-basketball: {
-  teams: 'grizzlies'
-position: {
-  pg: 3
-  c: 2
-  sf: 4
-  pf: 3
-  sg: 2
-}
+  basketball:{
+      teams: 'grizzlies',
+    position: [
+        pg: 3,
+        c: 2,
+        sf: 4,
+        pf: 3,
+        sg: 2
+      ]
+  },
+
+  hockey: {
+      teams: 'predators',
+    position: [
+        lw: 3,
+        rw: 4,
+        goalie: 2
+        ]
+  }
+
 }
 
-hockey: {
-  teams: 'predators'
-position: {
-  lw: 3
-  rw: 4
-  goalie: 2
-}
-}
-
-}
+p tn_sports
