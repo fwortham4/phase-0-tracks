f2ce585f0c56a1d10d230a9dc3e79d15537472dc
fwortham4 <forrest.wortham@gmail.com>
2016-05-12 17:58:56 -0500

    Created nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,36 @@
+#5.5 Nested Data Structures
+#by Forrest Wortham
+#
+
+prof_sports = {
+football: {
+  teams: 'titans',
+  position: {
+    qb: 3,
+    rb: 5
+    ol: 9
+    wr: 11
+    }
+    }
+
+basketball: {
+  teams: 'grizzlies'
+position: {
+  pg: 3
+  c: 2
+  sf: 4
+  pf: 3
+  sg: 2
+}
+}
+
+hockey: {
+  teams: 'predators'
+position: {
+  lw: 3
+  rw: 4
+  goalie: 2
+}
+}
+
+}
