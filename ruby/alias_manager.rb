9e53415c8a405740afc8425b1a392fd18272e2a3
fwortham4 <forrest.wortham@gmail.com>
2016-05-15 16:26:27 -0500

    Completed 5.6 Solo Project.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,21 +1,96 @@
 #5.6 Solo Challenge: Manipulating Strings with Iteration
 #by Forrest Wortham
 
-puts "What is the spy's full name?"
+directory = {}
 
-#takes user input of spy's full name
+puts "What is the spy's full name?\n"
+
 full_name = gets.chomp.downcase
+# Takes user input of spy's full name
 
-#splits full name into array["first", "last"]
-#.rotate inverts the first and last name within the array.
-reverse_name = full_name.split(' ').rotate.join(' ')
+def name_encryption(full_name)
 
-vowels = 'aeiou'
+full_name = full_name.split(' ').rotate.join(' ')
+# .split(' ') splits full_name into array ["first", "last"]
+# .rotate rotates the first and last name within the array. (Ex. ["last", "first"])
+# .join(' ') combines array into a string. (Ex. "last first")
+
+code_name = full_name.split('').map{ |x| x = next_letter(x)}
+# .split('') splits the string "last first" into individual characters.
+# .map{ |x| x = next_letter(x)} uses the next_letter method on each character and saves the changes.
+
+code_name = code_name.join('')
+# .join('') combines individual characters into a
+
+end
+
+
+
+def next_letter(x)
+# Method that determines vowle from consonants.
+
+vowel = 'aeiou'
 consonant = 'bcdfghjklmnpqrstvwxyz'
-index = 0
+# Created variables to represent vowels and consonants.
 
-reverse_name.chars
+if x == 'u'
+    x = 'a'
 
-puts reverse_name
+  elsif x == 'z'
+    x = 'b'
+    # Connects the ends of the vowels and consonants variabels; full-circle.
 
-#reverse_name.chars.map!{|consonant| consonant.next}.join('')
+  elsif vowel.include?(x)
+  # If x is a vowel
+
+    i = vowel.index(x)
+    # find its index with in the vowel variable.
+    x = vowel[i + 1]
+    # replace x with the next vowel
+
+  elsif consonant.include?(x)
+  # If x is a consonant.
+
+    i = consonant.index(x)
+    # Finds its index within the consonant variable.
+
+    x = consonant[i + 1]
+    # Replaces x with the next consonant.
+
+  else
+    x
+  # Skip all non-letters characters.
+
+  end
+
+end
+
+# Calls the method name_encryption on full_name.
+code_name = name_encryption(full_name)
+puts  "\n#{full_name}'s new code name is: #{code_name}\n"
+directory[code_name] = full_name
+
+# Loop to create multiple code_names
+loop do
+puts "\nWould you like to create another code name? (type quit when you're done).\n"
+full_name = gets.chomp
+
+if
+  full_name == 'quit'
+  break
+
+else
+  code_name = name_encryption(full_name)
+  puts  "\n#{full_name}'s new code name is: #{code_name}\n"
+
+  directory[code_name] = full_name
+  #Adds full_name and code_name to directory array.
+
+end
+end
+
+directory.each { |code_name, full_name|
+puts "#{full_name}'s new code name is: #{code_name}"
+}
+# Directory store all the full_name inputs and created code_name s and prints them all out at the end.
+
