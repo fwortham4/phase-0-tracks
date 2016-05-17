657d2c0b3dd698a6f3a014c9b0b9546a79dda293
fwortham4 <forrest.wortham@gmail.com>
2016-05-17 18:21:33 -0500

    Added simple Puppy class to puppy.rb

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -12,3 +12,18 @@
 # Fetch
 # Rollover
 # Play_Dead
+
+class Puppy
+end
+
+Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+spot.class
+duchess == fido
+fido.instance_of?(Array)
+fido.instance_of?(Puppy)
+spot.play_dead
