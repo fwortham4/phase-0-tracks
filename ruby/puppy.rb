62bb64ace002e88b8537f20d7bd3897e492fa615
fwortham4 <forrest.wortham@gmail.com>
2016-05-17 18:12:08 -0500

    Created pseudocoding for Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Cuteness_Level: 11
+# Friendly: yes
+# Legs: 4
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Fetch
+# Rollover
+# Play_Dead
