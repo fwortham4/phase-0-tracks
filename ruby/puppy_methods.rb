2483ef2ddad37f0a2acf51ca2f1d3d6657235275
fwortham4 <forrest.wortham@gmail.com>
2016-05-18 17:32:52 -0500

    Completed Release 0 by adding 4 methods to the Puppy class.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(number)
+   number.times do p "Woof" end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    puts "The dog is #{7*human_years} years old in dog years!"
+  end
+
+  def chase_cars(color)
+    puts "The dog is chasing a #{color} car!"
+  end
+
 end
 
+spot = Puppy.new
+spot.fetch("ball")
+spot.speak(3)
+spot.roll_over
+spot.dog_years(5)
+spot.chase_cars("red")
