7336af6e1ecd3662e23b461e954b2c124294e708
fwortham4 <forrest.wortham@gmail.com>
2016-05-20 17:46:25 -0500

    Added weight and origin attributes.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,15 @@
 # 6.3 Attributes
 # by Forrest Wortham
 
+
+
 class Santa
 
 reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
 age = 0
+
+# gender, ethnicity, weight, origin
 
   def initialize(gender, ethnicity, weight, origin)
     p "Initializing Santa instance ..."
@@ -14,6 +18,8 @@ age = 0
     @weight = weight
     @origin = origin
   end
+
+santas = []
 
   def speak
     p "Ho, ho, ho! Haaaappy holidays!"
@@ -30,13 +36,17 @@ puts "This is where the santa driver beigns."
 # santa1.speak
 # santa1.eat_milk_and_cookies("chocolate chip cookie")
 
-
 santas_array = []
+
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas_array << Santa.new(example_genders[i])
-end
 example_weight = [0, 50, 100, 150, 200, 250, 300]
 example_origin = ["Memphis", "Toronto", "Chicago", "New York", "Nashville", "Atlantis", "the Moon"]
-santa = Santa.new(example_genders, example_ethnicities,example_weight,example_origin)
+
+example_genders.length.times do |i|
+
+santas_array << Santa.new(example_genders[i], example_ethnicities[i], example_weight[i], example_origin[i])
+end
+
+p santas_array
+# Array.new(3, true) #=> [true, true, true]
