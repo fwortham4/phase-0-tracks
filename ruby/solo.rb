9dc62d16114c54b917f25a7d64805b32321115b6
fwortham4 <forrest.wortham@gmail.com>
2016-05-22 00:41:53 -0500

    Completed Release2 by creating the user interface.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,7 +7,6 @@
 # attribute1: name
 # attribute2: o2_level
 # attribute3: depth
-# attribute4: dive_time
 
 #method1: max_depth
   # records maximum depth
@@ -19,13 +18,12 @@
 class Scuba_Diver
 
 attr_reader :name, :o2_level, :depth, :dive_time
-attr_accessor :o2_level, :depth, :dive_time
+attr_accessor :o2_level, :depth
 
   def initialize(name)
     @name = name
     @o2_level = 3000
     @depth = 0
-    @dive_time = dive_time
   end
 
   def max_depth()
@@ -42,7 +40,7 @@ end
 ############# Driver Code ############
 
 scuba_diver_array = []
-name_list = ["Forrest", "Bob", "Sally", "Sue", "Jim"]
+name_list = []
 name_list.length.times do |i|
 end
 
@@ -50,23 +48,49 @@ i = name_list.length
 index=0
 scuba_diver_array = []
 
+############# User Interface ############
+loop do
+puts "Please provide a name for Diver #{index+1} or type the word done when done"
+
+input = gets.chomp.capitalize
+
+if input == 'Done'
+  p scuba_diver_array << Scuba_Diver.new("")
+break
+
+else
+name_list << input
+# p name_list
+# scuba_diver_array << Scuba_Diver.new(input)
+# p scuba_diver_array
+end
+#########################################
+
+############# Driver Code ############
 puts "There are now #{name_list.length} divers in the water.\n\n"
 
-i.times do
-scuba_diver_array << Scuba_Diver.new("Forrest")
+# The Iterator
+# i.times do
+scuba_diver_array << Scuba_Diver.new(input)
 
+#Name
 puts "Diver#: #{index+1} is named #{name_list[index]}"
 
+#O2 Start
 puts "Diver#: #{index+1} started with #{scuba_diver_array[index].o2_level} psi of air in their tank."
+
+#Run Methods
 scuba_diver_array[index].o2_check
 
 scuba_diver_array[index].max_depth
 
+#O2 Ending
 puts "Diver#: #{index+1} now has #{scuba_diver_array[index].o2_level} psi of air left in their tank."
+
+#Max Depth
 puts "Diver#: #{index+1} reached a maximum depth of #{scuba_diver_array[index].depth}ft.\n\n"
 
 index+=1
 end
-
+# end
 ######################################
-
