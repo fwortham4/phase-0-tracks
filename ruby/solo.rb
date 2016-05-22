82f21efdaaec8437f45ebd9c5576e441b7c7fba1
fwortham4 <forrest.wortham@gmail.com>
2016-05-21 22:32:11 -0500

    Pseudo coded Scuba_Diver class

---
+++ ruby/solo.rb
@@ -0,0 +1,12 @@
+#6.4 Solo Challenge: Designing and Implementing a Class
+# by Forrest Wortham
+
+#class Scuba_Diver
+# attribute1: name
+# attribute2: o2_level
+# attribute3: depth
+
+#method1: descend
+  # changes the depth
+#method2: check_o2_level
+  # updates o2_level
