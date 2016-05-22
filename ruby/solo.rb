9ea4e60dfca11e7049e62d3c3d0a2dedafb34132
fwortham4 <forrest.wortham@gmail.com>
2016-05-21 22:37:47 -0500

    Built Scuba_Diver class, added initialize method with instance variables, and add attr_reader and attr_accessor.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,8 @@
 #6.4 Solo Challenge: Designing and Implementing a Class
 # by Forrest Wortham
 
+############# Pseudo Code ############
+
 #class Scuba_Diver
 # attribute1: name
 # attribute2: o2_level
@@ -10,3 +12,18 @@
   # changes the depth
 #method2: check_o2_level
   # updates o2_level
+
+#######################################
+
+class Scuba_Diver
+
+attr_reader :name
+attr_accessor :o2_level, :depth
+
+  def initialize
+    @name = name
+    @o2_level = o2_level
+    @depth = depth
+  end
+
+end
