4d2c3d23ac7e9c49f01a04925b86f3e0556c2d81
fwortham4 <forrest.wortham@gmail.com>
2016-05-21 23:51:51 -0500

    Completed driver code.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,23 +7,66 @@
 # attribute1: name
 # attribute2: o2_level
 # attribute3: depth
+# attribute4: dive_time
 
-#method1: descend
-  # changes the depth
-#method2: check_o2_level
+#method1: max_depth
+  # records maximum depth
+#method2: o2_check
   # updates o2_level
 
-#######################################
+######################################
 
 class Scuba_Diver
 
-attr_reader :name
-attr_accessor :o2_level, :depth
+attr_reader :name, :o2_level, :depth, :dive_time
+attr_accessor :o2_level, :depth, :dive_time
 
-  def initialize
+  def initialize(name)
     @name = name
-    @o2_level = o2_level
-    @depth = depth
+    @o2_level = 3000
+    @depth = 0
+    @dive_time = dive_time
   end
 
+  def max_depth()
+    @depth = rand(0..150)
+  end
+
+  def o2_check()
+    @o2_level = @o2_level-rand(0..3000)
+  end
 end
+
+######################################
+
+############# Driver Code ############
+
+scuba_diver_array = []
+name_list = ["Forrest", "Bob", "Sally", "Sue", "Jim"]
+name_list.length.times do |i|
+end
+
+i = name_list.length
+index=0
+scuba_diver_array = []
+
+puts "There are now #{name_list.length} divers in the water.\n\n"
+
+i.times do
+scuba_diver_array << Scuba_Diver.new("Forrest")
+
+puts "Diver#: #{index+1} is named #{name_list[index]}"
+
+puts "Diver#: #{index+1} started with #{scuba_diver_array[index].o2_level} psi of air in their tank."
+scuba_diver_array[index].o2_check
+
+scuba_diver_array[index].max_depth
+
+puts "Diver#: #{index+1} now has #{scuba_diver_array[index].o2_level} psi of air left in their tank."
+puts "Diver#: #{index+1} reached a maximum depth of #{scuba_diver_array[index].depth}ft.\n\n"
+
+index+=1
+end
+
+######################################
+
