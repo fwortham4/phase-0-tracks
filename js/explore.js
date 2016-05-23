844c78666141f5b1f7bb87f87ad68c99ec59a0cb
fwortham4 <forrest.wortham@gmail.com>
2016-05-23 17:53:42 -0500

    Created a file explore.js and added pseudo coding for reverse string function.

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// 7.1 Syntax - Release4
+// by Forrest Wortham
+
+///////// Pseudo Code ////////
+// create a variable that is a string.
+// create a function that receives a string as a parameter.
+// Returns a the string in reverse order.
