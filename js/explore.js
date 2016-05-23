51411d5e6f262a1d8472a8cab6125d50119763ad
fwortham4 <forrest.wortham@gmail.com>
2016-05-23 18:39:29 -0500

    created string and function, but struggling to build an array of the individual letters of the string.

--- js/explore.js
+++ js/explore.js
@@ -2,6 +2,23 @@
 // by Forrest Wortham
 
 ///////// Pseudo Code ////////
-// create a variable that is a string.
-// create a function that receives a string as a parameter.
-// Returns a the string in reverse order.
+// 1.) create a variable that is a string.
+// 2.) create a function that receives a string as a parameter.
+// 3.) separates the string into individual letters.
+// 4.) Reverse the letters
+// 5.) Returns a the new string in reverse order.
+
+var string = "hello";
+
+function reverse(string) {
+  for(var i = 0; i<string.length; i++) {
+    var letter = string[i];
+    var array = [];
+    array.push(letter);
+    console.log(array);
+  }
+}
+
+reverse(string);
+
+    // console.log(reverse_string);
