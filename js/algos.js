88bb3d1f0341499fd0e2a767a94904dddf48aee5
fwortham4 <forrest.wortham@gmail.com>
2016-05-25 22:56:31 -0500

    Able to return longest phrase, but a random 3 is also returned.

--- js/algos.js
+++ js/algos.js
@@ -3,21 +3,42 @@
 
 /////////// Pseudo Coding ///////////
 // 1.) define the function that takes an array as an arguement.
-// 2.) determine the length of each element of the array.
-// 3.) print out the longest element of the array as the result.
+// 2.) determine the length of each string of the input array.
+// 3.) Place element lengths in a new array.
+// 4.) Determine the largest integer in the new array.
+// 5.) Return the string of the largest integer in the new array.
+// 6.) Return the index of the largest integer in the new array.
+// .) print out the longest element of the input array as the result.
 /////////////////////////////////////
 
-/////////// Driver Code /////////////
+/////////// Method /////////////
+
+// 1.)
 function longest_phrase(array) {
   console.log(array.length)
   var element_length = [];
+
+// 2.)
   for (i=0;i<array.length;i++) {
+
+// 3.)
     element_length.push(array[i].length);
 
+// 4.)
+    var longest_string = Math.max(...element_length);
   }
-  console.log(element_length);
+
+  var longest_phrase_index = element_length.indexOf(longest_string);
+
+// 5.)-7.)
+  console.log(array[longest_phrase_index])
 }
+//////////////////////////////////////
 
-var input_array = ["long phrase","longest phrase","longer phrase"]
-longest_phrase(input_array);
 
+/////////// Driver Code /////////////
+
+longest_phrase(["long phrase","longest phrase","longer phrase"]);
+//////////////////////////////////////
+
+//  .findIndex(longest_phrase)
