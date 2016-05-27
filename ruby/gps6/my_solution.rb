bc14e4b8580862e1a7e8a9d2622ccbcb959d0b3d
fwortham4 <forrest.wortham@gmail.com>
2016-05-27 00:49:50 -0500

    Completed GPS 3.1

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,29 +1,38 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [ with: Ryan ].
+# We spent [2] hours on this challenge.
 
 # EXPLANATION OF require_relative
 #
-#
+# The require_relative is connecting the data in state_data.rb with the same relative path as the code in this file.
+
 require_relative 'state_data'
 
 class VirusPredictor
 
+# Defines instance variables and connects all the other methods in the class.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+#private
+# This method is used to call both the other 2 methods at the same time.
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    #(@population_density, @population, @state)
+    speed_of_spread
+    #(@population_density, @state)
   end
 
-  private
+  #private
 
-  def predicted_deaths(population_density, population, state)
+  # Private - private methods can only be called within the class because they can be access by another method (virus-effect). Making the code quite simpler.
+
+# Calculating/predicting the number of deaths based on the pre-existing formula.
+  def predicted_deaths
+  #(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +50,9 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+# Calculating the rate of deaths with formula using population density.
+  def speed_of_spread
+  #(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -69,19 +80,42 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+# STATE_DATA.each do |state_of_origin, population_hash|
+#   p state_of_origin
+# state_of_origin = VirusPredictor.new("#{state_of_origin}", STATE_DATA["#{state_of_origin}"], population_hash[:population_density], STATE_DATA["#{state_of_origin}"][:population])
+# end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+STATE_DATA.each do |state_origin, state_info|
+state = VirusPredictor.new(state_origin, state_info[:population_density], state_info[:population])
+state.virus_effects
+end
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
+
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 
 #=======================================================================
-# Reflection Section
+################## Reflection Section ##################
+# 1.) What are the differences between the two different hash syntaxes shown in the state_data file?
+### State_Data is a large hash with smaller hashes of each state, population, and population density nested inside. Also State_Data is a constant (ALL CAPS) variable that should not be changed by the code.
+
+# 2.) What does require_relative do? How is it different from require?
+### Require_relative links the information in  the state_data.rb file with the code being run in my_solutions.
+
+# 3.) What are some ways to iterate through a hash?
+###
+
+# 4.) When refactoring virus_effects, what stood out to you about the variables, if anything?
+### That they were redundant and could be refactored.
+
+# 5.) What concept did you most solidify in this challenge?
+### Insstance Variable. Figured out they can be called in any method inside the class where they are defined without be put in the method input.
