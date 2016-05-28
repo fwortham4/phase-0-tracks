e35cbb792fe8bc14da31b037c4fc31fd16b0c9ee
fwortham4 <forrest.wortham@gmail.com>
2016-05-28 12:10:29 -0500

    Created data_structure.js file and added 2 arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+// 7.2 Mandatory Pairing: Data Structures
+// by Forrest Wortham and
+
+var blue_array = ["aqua", "teal", "dark blue", "turquiose"]
+var horse_names = ["Bubba", "Joker", "Lebron", "Princess"]
+
