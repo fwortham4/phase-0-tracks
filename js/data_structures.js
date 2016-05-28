12129e01ea2bdac6385c5bf6b9f86f6e4478bf35
fwortham4 <forrest.wortham@gmail.com>
2016-05-28 13:03:42 -0500

    Added notes about Construction Functions; created construction function called Car.

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,6 +17,7 @@ console.log(horse_names);
 //////////////// Release1 ////////////////////
 // JS : "Objects" <-> Ruby : "Hashes"
 // Keys = horse_names and Values = blue_array
+// Literal Construction of an Object.
 
 var blue_horses = {};
 
@@ -27,4 +28,26 @@ for (var i=0;i<horse_names.length;i++) {
 console.log(blue_horses);
 
 //////////////// Release2 ////////////////////
+// Jave Script Obejects:
 
+// 1.) Objects can have behavior. In other words, objects can have functions as values.
+
+// 2.) Objects can be produced from a blueprint or prototype, loosely similar to the way instances in Ruby can be produced from a class.
+
+// Constructor Functions (uppercase functions) is similar to initializing a classes in Ruby.
+// JS: 'this' <-> Ruby: @name
+// Functions can be used as Value anytime.
+
+
+// Adding a new behavior -> Ex.) <object>.<newBehavior> = function() { <Code for new behavior>};
+
+function Car(brand, year, model, zeroTo60) {
+  this.brand = brand;
+  this.year = year;
+  this.model = model;
+  this zeroTo60 = zeroTo60;
+
+  this.zoom = function() {console.log("Zoom ZOOM!!!");};
+
+  console.log("CAR INITIALIZATION COMPLETE");
+}
