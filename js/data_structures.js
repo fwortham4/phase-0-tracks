be71f1bb1ac7c85634e61a6794fafa738ce2f32a
fwortham4 <forrest.wortham@gmail.com>
2016-05-28 12:20:05 -0500

    Completed Release1; added new elements to the blue and horse name arrays.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,14 @@
 // 7.2 Mandatory Pairing: Data Structures
 // by Forrest Wortham and
 
-var blue_array = ["aqua", "teal", "dark blue", "turquiose"]
-var horse_names = ["Bubba", "Joker", "Lebron", "Princess"]
+// Release1
+var blue_array = ["aqua", "teal", "dark blue", "turquiose"];
+var horse_names = ["Bubba", "Joker", "Lebron", "Princess"];
 
+blue_array.push("blue steel");
+horse_names.push("Zoolander");
+
+console.log(blue_array);
+console.log(horse_names);
+
+// Release2
