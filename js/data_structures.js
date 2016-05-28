23c77e0536a2b72179c8ec44c275e42dc117af15
fwortham4 <forrest.wortham@gmail.com>
2016-05-28 12:41:32 -0500

    Completed Release1 by looping through bracket notation of adding key-value pairs to an object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,10 @@
 // 7.2 Mandatory Pairing: Data Structures
 // by Forrest Wortham and
 
-// Release0
+//////////////// Release0 ////////////////////
+// 1.) Create 2 arrays.
+// 2.) Add new elements to both arrays.
+
 var blue_array = ["aqua", "teal", "dark blue", "turquiose"];
 var horse_names = ["Bubba", "Joker", "Lebron", "Princess"];
 
@@ -11,4 +14,17 @@ horse_names.push("Zoolander");
 console.log(blue_array);
 console.log(horse_names);
 
-// Release1
+//////////////// Release1 ////////////////////
+// JS : "Objects" <-> Ruby : "Hashes"
+// Keys = horse_names and Values = blue_array
+
+var blue_horses = {};
+
+for (var i=0;i<horse_names.length;i++) {
+  blue_horses[horse_names[i]] = blue_array[i];
+};
+
+console.log(blue_horses);
+
+//////////////// Release2 ////////////////////
+
